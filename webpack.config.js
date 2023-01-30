a76f7cbdeea2d52415575ada4fed0bb3d53b15b5
4n6r35 <andresinge07@gmail.com>
2023-01-30 14:13:53 -0500

    webpack-base

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+
+const HTMLWebPack = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+    mode: 'development',
+
+    output: {
+        clean: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /style.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /style.css$/,
+                use: [MiniCssExtract.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+
+    optimization: {},
+
+    plugins: [
+        new HTMLWebPack({
+            title: 'My Webpack App',
+            // filename: 'holamundo.html'
+            template: 'src/index.html'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets/', to: 'assets/' }
+            ]
+        })
+    ],
+}
